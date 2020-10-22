8e917debd3336d30e636513960c0c978a66356d0
RaihanAkter03 <raihanakter26@gmail.com>
2020-10-22 18:04:03 -0500

    added asset folder , additional code

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Server-Side-APIs-Weather-Dashboard
-```
 
+# Server-Side-APIs-Weather-Dashboard
 
 https://raihanakter03.github.io/Server-Side-APIs-Weather-Dashboard
+
+
 https://github.com/RaihanAkter03/Server-Side-APIs-Weather-Dashboard
 
 
 ## User Story
 
-```
 AS A traveler
 I WANT to see the weather outlook for multiple cities
 SO THAT I can plan a trip accordingly
-```
+
 
 ## Acceptance Criteria
 
@@ -31,4 +31,14 @@ THEN I am again presented with current and future conditions for that city
 WHEN I open the weather dashboard
 THEN I am presented with the last searched city forecast
 ```
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+![Assets](./Assets/weatherdashboard.gif)
+
+## Description
+```
+ A weather dashboard with form inputs
+ 1. search for a city you will presented with current and future conditions for that city and that city is added to the search history.
+ 2. view current weather conditions for that city ,presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+3. view the UV index ,presented with a color that indicates whether the conditions are low for green , moderate for yellow , high for orange, very high for red and extrem for violet.
+4. view future weather conditions for that city, presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+5. click on a city in the search history , presented with current and future conditions for that city
+6. open the weather dashboard , presented with the last searched city forecast
--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     var WindSpeed = "";
     var UV = "";
 
+    clear();
     
     if (arr.length > 0) {
         buildQueryURL(arr[arr.length - 1]);
@@ -24,6 +25,7 @@ $(document).ready(function () {
         searchTerm = $("#search-field").val().trim();
         console.log(searchTerm);
         buildQueryURL(searchTerm);
+        // Forecast(searchTerm);
       
         if (arr.indexOf(searchTerm) === -1) {
             arr.push(searchTerm);
@@ -43,14 +45,14 @@ $(document).ready(function () {
             event.preventDefault();
             name = $(this).text();
             console.log(name);
-            buildQueryURL(name);// query build function  
-           
+            buildQueryURL(name);// query build function             
         });
     }
 
     function buildQueryURL(cityName) {
         console.log(cityName);
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=6812942cb210c8101a17ceb4d5f76524";
+        // queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=6812942cb210c8101a17ceb4d5f76524";
+         queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=6812942cb210c8101a17ceb4d5f76524";
 
         $.ajax({
             type: "GET",
@@ -58,18 +60,43 @@ $(document).ready(function () {
             dataType: "json",
             success: function (response) {
                 console.log(response);
-                tempF = response.main.temp;
-                humidity = response.main.humidity;
-                WindSpeed = response.wind.speed;
+                tempF = response.list[0].main.temp;
+                humidity = response.list[0].main.humidity;
+                WindSpeed = response.list[0].wind.speed;
                             
-                localStorage.setItem("lon", JSON.stringify(response.coord.lon));
-                localStorage.setItem("lat", JSON.stringify(response.coord.lat));
+                localStorage.setItem("lon", JSON.stringify(response.city.coord.lon));
+                localStorage.setItem("lat", JSON.stringify(response.city.coord.lat));
                          
-                var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+                var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
                 
-                LocationDisplay(response.name, tempF, humidity, WindSpeed, img);
+                var img1 = $(".image1").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                
+                var img2 = $(".image2").attr("src", "http://openweathermap.org/img/w/" + response.list[2].weather[0].icon + ".png");
+                
+                var img3 = $(".image3").attr("src", "http://openweathermap.org/img/w/" + response.list[3].weather[0].icon + ".png");
+                
+                var img4 = $(".image4").attr("src", "http://openweathermap.org/img/w/" + response.list[4].weather[0].icon + ".png");
+                
+                var img5 = $(".image5").attr("src", "http://openweathermap.org/img/w/" + response.list[4].weather[0].icon + ".png");
+
+                var T1 = response.list[0].main.temp;
+                var H1 = response.list[0].main.humidity;
+
+                var T2 = response.list[1].main.temp;
+                var H2 = response.list[1].main.humidity;
+
+                var T3 = response.list[2].main.temp;
+                var H3 = response.list[2].main.humidity;
+
+                var T4 = response.list[3].main.temp;
+                var H4 = response.list[3].main.humidity;
+                
+                var T5 = response.list[4].main.temp;
+                var H5 = response.list[4].main.humidity;
+                // current day weather
+                LocationDisplay(response.city.name, tempF, humidity, WindSpeed, img);
             
-                Forecast(img);// 5 day forecast function
+                Forecast(img1, img2, img3, img4, img5, T1, T2, T3, T4, T5, H1, H2, H3, H4, H5);// 5 day forecast function
             }
 
         });
@@ -85,13 +112,27 @@ $(document).ready(function () {
             success: function (response) {
                 UV = response.value;
                 var pE4 = $(".news4");
+                                
+                if (UV < 2 && UV === 0) {
+                    pE4.addClass("low");  
+                }
+                if (UV > 2 && UV === 5) { 
+                    pE4.addClass("moderate");
+                }
+                if (UV > 5 && UV < 7) { 
+                    pE4.addClass("high");
+                }
+                if (UV > 8 && UV < 10) {
+                    pE4.addClass("veryHigh");
+                }
+                if (UV > 10 ) {
+                    pE4.addClass("extreme");
+                }
                 pE4.text("UV Index: " + UV);
                 $("#Weather").append(pE4);
             }
         });
     }
-
-    
 
     function LocationDisplay(name, tempF, humidity, WindSpeed, img)
     { 
@@ -117,73 +158,92 @@ $(document).ready(function () {
         $("#Weather").append(pE3);  
     }
 
-    function Forecast(image) { 
+    function Forecast(img1, img2, img3, img4, img5, T1, T2, T3, T4, T5, H1, H2, H3, H4, H5) { 
 
-        var day1 = $(".day1");
-        day1.text(moment().format('L')).append(image);
-        $("#forecast1").append(day1);
-        
-        // $("#forecast2").append(imgE);
+                //date
+                var day1 = $(".day1");
+                day1.text(moment().format('L'));
+                $("#forecast1").append(day1);
 
-        var day2 = $(".day2");
-        day2.text(moment().add(1, 'days').format('L')).append(image);
-        $("#forecast2").append(day2);
+                // $("#forecast2").append(imgE);
 
-        var day3 = $(".day3");
-        day3.text(moment().add(2, 'days').format('L')).append(image);
-        $("#forecast3").append(day3);
+                var day2 = $(".day2");
+                day2.text(moment().add(1, 'days').format('L'));
+                $("#forecast2").append(day2);
 
-        var day4 = $(".day4");
-        day4.text(moment().add(3, 'days').format('L')).append(image);
-        $("#forecast4").append(day4);
+                var day3 = $(".day3");
+                day3.text(moment().add(2, 'days').format('L'));
+                $("#forecast3").append(day3);
 
-        var day5 = $(".day5");
-        day5.text(moment().add(4, 'days').format('L')).append(image);
-        $("#forecast5").append(day5);
-       
-        // $("#forecast1").append(image1);      
-        // $("#forecast2").append(image2); 
-        // $("#forecast3").append(image3); 
-        // $("#forecast4").append(image4); 
-        // $("#forecast5").append(image5); 
+                var day4 = $(".day4");
+                day4.text(moment().add(3, 'days').format('L'));
+                $("#forecast4").append(day4);
 
-        var temp1 = $(".forecast1-temp");
-        var humi1 = $(".forecast1-humi");
-        temp1.text("Temp: " + tempF + " F");
-        humi1.text("Humidity: "+humidity+"%");
-        $("#forecast1").append(temp1);
-        $("#forecast1").append(humi1);
+                var day5 = $(".day5");
+                day5.text(moment().add(4, 'days').format('L'));
+                $("#forecast5").append(day5);
 
-        var temp2 = $(".forecast2-temp");
-        var humi2 = $(".forecast2-humi");
-        temp2.text("Temp: " + tempF + " F");
-        humi2.text("Humidity: " + humidity + "%");
-        $("#forecast2").append(temp2);
-        $("#forecast2").append(humi2);
+                //image
+ 
+                $("#forecast1").append(img1);
 
-        var temp3 = $(".forecast3-temp");
-        var humi3 = $(".forecast3-humi");
-        temp3.text("Temp: " + tempF + " F");
-        humi3.text("Humidity: " + humidity + "%");
-        $("#forecast3").append(temp3);
-        $("#forecast3").append(humi3);
+                $("#forecast2").append(img2);
 
-        var temp4 = $(".forecast4-temp");
-        var humi4 = $(".forecast4-humi");
-        temp4.text("Temp: " + tempF + " F");
-        humi4.text("Humidity: " + humidity + "%");
-        $("#forecast4").append(temp4);
-        $("#forecast4").append(humi4);
+                $("#forecast3").append(img3);
 
-        var temp5 = $(".forecast5-temp");
-        var humi5 = $(".forecast5-humi");
-        temp5.text("Temp: " + tempF + " F");
-        humi5.text("Humidity: " + humidity + "%");
-        $("#forecast5").append(temp5);
-        $("#forecast5").append(humi5);
+                $("#forecast4").append(img4);
+
+                $("#forecast5").append(img5);
+
+                //temperature and humidity..
+
+                var temp1 = $(".forecast1-temp");
+                var humi1 = $(".forecast1-humi");
+                temp1.text("Temp: " + T1 + " F");
+                humi1.text("Humidity: " + H1 + "%");
+                $("#forecast1").append(temp1);
+                $("#forecast1").append(humi1);
 
 
+                var temp2 = $(".forecast2-temp");
+                var humi2 = $(".forecast2-humi");
+                temp2.text("Temp: " + T2 + " F");
+                humi2.text("Humidity: " + H2 + "%");
+                $("#forecast2").append(temp2);
+                $("#forecast2").append(humi2);
 
+
+                var temp3 = $(".forecast3-temp");
+                var humi3 = $(".forecast3-humi");
+                temp3.text("Temp: " + T3 + " F");
+                humi3.text("Humidity: " + H3 + "%");
+                $("#forecast3").append(temp3);
+                $("#forecast3").append(humi3);
+
+
+                var temp4 = $(".forecast4-temp");
+                var humi4 = $(".forecast4-humi");
+                temp4.text("Temp: " + T4 + " F");
+                humi4.text("Humidity: " + H4 + "%");
+                $("#forecast4").append(temp4);
+                $("#forecast4").append(humi4);
+
+
+                var temp5 = $(".forecast5-temp");
+                var humi5 = $(".forecast5-humi");
+                temp5.text("Temp: " + T5 + " F");
+                humi5.text("Humidity: " + H5 + "%");
+                $("#forecast5").append(temp5);
+                $("#forecast5").append(humi5);
+
+
+            // }
+        // });
+    }
+    // Function to empty out the articles
+    function clear() {
+        $(".list-group").empty();
+        $(".list-group-item").empty();
     }
 
     
